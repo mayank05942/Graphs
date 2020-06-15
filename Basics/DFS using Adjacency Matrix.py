9b2bbe3de9ce3e4467d285b6a90081bd26ce5edd
Mayank Nautiyal <57125483+mayank05942@users.noreply.github.com>
2020-06-15 13:03:21 +0530

    Update DFS using Adjacency Matrix.py

--- Basics/DFS using Adjacency Matrix.py
+++ Basics/DFS using Adjacency Matrix.py
@@ -2,30 +2,26 @@ V = int(input("Enter number of Vertex:")) # no of vertex
 E = int(input("Enter number of Edges:"))   # No of egdes
 
 adj = [[0]*V for _ in range(V)] # V x V matrix
-
-
-
 # Mention the starting vertex
-
-
 def dfs(adj,sv,visited):
+    print(sv)
     num_vertex = len(adj)
     visited[sv] = True
     for i in range(num_vertex): # go to every vertex
-        if adj[sv][i] == 1 and visited[i] is False:
-            #print(i)
+        if adj[sv][i] == 1:
+            if visited[i] is True:
+                continue
+
             dfs(adj,i,visited)
 
 def print_graph(adj):
+
     visited = [False]* (len(adj))
 
     for i in range(len(adj)):
         if visited[i] == False:
-            
+
             dfs(adj,i,visited)
-
-
-
 
 for _ in range(E):
 
